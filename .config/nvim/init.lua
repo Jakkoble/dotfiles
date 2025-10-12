510640c832a330e144e5c2a40404909d1d0372f6
Jakkoble <hey@jakob.foo>
2025-10-12 17:39:52 +0200

    added relative numbers

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -4,6 +4,13 @@ vim.cmd("set expandtab")
 vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
+
 vim.diagnostic.config({ virtual_text = true })
+
+vim.opt.number = true
+vim.wo.relativenumber = true
+vim.opt.cursorline = true
+vim.opt.cursorlineopt = "number"
+vim.api.nvim_set_hl(0, "CursorLineNr", { fg = "#ffaf00", bold = true })
 
 require("config.lazy")
