c9b28217d2f2716497a55d3d975338bc862c1a2d
Jakkoble <hey@jakob.foo>
2025-10-17 19:53:33 +0200

    added plugins for webdev environment

---
+++ .config/nvim/lua/plugins/conform.lua
@@ -0,0 +1,28 @@
+return {
+	"stevearc/conform.nvim",
+	opts = {},
+	config = function()
+		local conform = require("conform")
+
+		conform.setup({
+			formatters_by_ft = {
+				javascript = { "prettier" },
+				typescript = { "prettier" },
+				javascriptreact = { "prettier" },
+				typescriptreact = { "prettier" },
+				css = { "prettier" },
+				html = { "prettier" },
+				json = { "prettier" },
+				yaml = { "prettier" },
+				markdown = { "prettier" },
+			},
+		})
+
+		vim.api.nvim_create_autocmd("BufWritePre", {
+			pattern = "*",
+			callback = function(args)
+				conform.format({ bufnr = args.buf })
+			end,
+		})
+	end,
+}
