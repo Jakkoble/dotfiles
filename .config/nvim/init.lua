36bfa63dc8babe56120a90b877218532e136b83f
Jakkoble <hey@jakob.foo>
2026-04-10 10:28:41 +0200

    add ghostty base configuration

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,16 +1,5 @@
-vim.g.mapleader = " "
-
-vim.cmd("set expandtab")
-vim.cmd("set tabstop=2")
-vim.cmd("set softtabstop=2")
-vim.cmd("set shiftwidth=2")
-
-vim.diagnostic.config({ virtual_text = true })
-
-vim.opt.number = true
-vim.wo.relativenumber = true
-vim.opt.cursorline = true
-vim.opt.cursorlineopt = "number"
-vim.api.nvim_set_hl(0, "CursorLineNr", { fg = "#ffaf00", bold = true })
+require("config.options")
+require("config.autocmds")
+dasd
 
 require("config.lazy")
